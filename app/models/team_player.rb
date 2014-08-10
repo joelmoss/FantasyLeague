1ff2495a9a710775c4fafaa43d421a592ef56663
Joel Moss <joel@developwithstyle.com>
2014-08-10 17:19:42 +0100

    Recaluculating budget after adding player to team

--- app/models/team_player.rb
+++ app/models/team_player.rb
@@ -8,4 +8,14 @@ class TeamPlayer < ActiveRecord::Base
   validates :team_id, presence: true
   validates :purchase_price, presence: true
 
+  after_create :recalculate_team_budget
+
+
+  private
+
+    # TODO: warn when recalculated budget goes below zero
+    def recalculate_team_budget
+      team.update_attribute :budget, team.budget - purchase_price
+    end
+
 end
