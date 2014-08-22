d865752badfcb6a37be238eb768805e79a44670e
Joel Moss <joel@developwithstyle.com>
2014-08-22 12:09:51 +0100

    Sending messages when player statuses change

--- app/models/player.rb
+++ app/models/player.rb
@@ -25,6 +25,7 @@ class Player < ActiveRecord::Base
   has_many :fixtures, through: :fixture_players
   has_many :sealed_bids, dependent: :destroy
 
+  after_update :notify_status_changes
   before_destroy :notify_manager_of_removal
 
   accepts_nested_attributes_for :team_player, :sealed_bids
@@ -74,16 +75,42 @@ class Player < ActiveRecord::Base
 
     def notify_manager_of_removal
       unless free_agent?
-        manager = TeamPlayer.unscoped { team_player }.team.manager
-        con = Conversation.create subject: "Player removed from your team: #{self}",
-                                  creator: manager,
-                                  recipient: manager
-        con.messages.create manager: manager,
-                            body: "Sorry to tell you that #{self} has been removed from the player "+
-                                  "list, and therefore has also been removed from your Team with "+
-                                  "immediate effect. Any previous points that he gained you in the "+
-                                  "past will remain in place."
+        body = "Sorry to tell you that #{self} has been removed from the player "+
+               "list, and therefore has also been removed from your Team with "+
+               "immediate effect. Any previous points that he gained you in the "+
+               "past will remain in place."
+        create_conversation "#{self} removed", body
       end
+    end
+
+    def notify_status_changes
+      if !free_agent?
+        subject = "#{self} status changed"
+        if !status_changed? && status_notes_changed?
+          if status_notes_was.nil?
+            body = "The status of #{self}, which is #{status} has been updated with a note: #{status_notes}"
+          elsif status_notes
+            body = "The status of #{self}, which is #{status} has been updated from #{status_notes_was} to #{status_notes}"
+          end
+          create_conversation subject, body
+        elsif status_changed?
+          if status_was.nil?
+            body = "The status of #{self} has changed to #{status}"
+          elsif status.nil?
+            body = "The status of #{self} has been cleared from #{status_was}. The player is now able to play."
+          else
+            body = "The status of #{self} has changed from #{status_was} to #{status}"
+          end
+          body += " (#{status_notes})" if status_notes
+          create_conversation subject, body
+        end
+      end
+    end
+
+    def create_conversation(subject, body)
+      manager = TeamPlayer.unscoped { team_player }.team.manager
+      con = Conversation.create subject: subject, creator: manager, recipient: manager
+      con.messages.create manager: manager, body: body
     end
 
 end
